996e5a0e130f15f66cef21c78f35d94297691657
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-06 21:54:04 -0300

    implement filmes salvos page

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./favoritos.css";
 
 function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    const minhaLista = localStorage.getItem("filmes");
+    setFilmes(JSON.parse(minhaLista) || []);
+  }, []);
+
+  function handleDelete(id) {
+    const filtroFilmes = filmes.filter((filme) => filme.id !== id);
+
+    localStorage.setItem("filmes", JSON.stringify(filtroFilmes));
+    setFilmes(filtroFilmes);
+  }
+
   return (
-    <div>
-      <h1>Favoritos</h1>
+    <div id="meus-filmes">
+      <h1>Meus Flimes</h1>
+
+      <ul>
+        {filmes.map((filme) => {
+          return (
+            <li key={filme.id}>
+              <span>{filme.nome}</span>
+              <div>
+                <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
+                <button onClick={() => handleDelete(filme.id)}>Excluir</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
